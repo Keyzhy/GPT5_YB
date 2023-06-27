1636819528b0bef0ff289881d99a65e2ccfd51cf
Keyzhy <yannisboulaid@protonmail.com>
2023-06-27 03:02:40 +0200

    Ajout des assets et Styling de la NavBar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
+import {RiMenu3Line, RiCloseLine} from 'react-icons/ri';
 import './navbar.css';
+import logo from '../../assets/logo.svg';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='gpt5__navbar'>
+      <div className='gpt5__navbar-links'>
+        <div className='gpt5__navbar-links_logo'>
+          <img src={logo} alt="logo" />
+        </div>
+        <div className='gpt5__navbar-links_container'>
+          <p><a href="#home">Home</a></p>
+          <p><a href="#wgpt5">What is GPT5?</a></p>
+          <p><a href="#possibility">Open AI</a></p>
+          <p><a href="#features">Case Studies</a></p>
+          <p><a href="#blog">Library</a></p>
+        </div>
+      </div>
+      <div className='gpt5__navbar-sign'>
+        <p>Sign In</p>
+        <button type='button'>Sign Up</button>
+      </div>
+    </div>
   )
 }
 
